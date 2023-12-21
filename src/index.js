6abb2e46e74752ca9ceea2ec2bf13035a85e5b35
Tohir Kuliev <kuliev.tech@gmail.com>
2023-12-21 16:06:24 -0500

    fix CORS issue

--- src/index.js
+++ src/index.js
@@ -14,6 +14,13 @@ const PORT = process.env.PORT || 4000;
 app.use("/students", router);
 app.use(GlobalError.handle);
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+});
+
 app.listen(PORT, () => {
     console.log("Server is running on ", PORT);
 });
