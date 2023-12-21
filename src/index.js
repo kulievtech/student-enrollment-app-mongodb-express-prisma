4f92c6538ed0b4cf9b25d8400d3c125858d52820
Tohir Kuliev <kuliev.tech@gmail.com>
2023-12-21 16:13:00 -0500

    fix another CORS problem

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import { router } from "./routes/routes.js";
 import { GlobalError } from "./errors/global-error.js";
 
+import cors from "cors";
+
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -11,15 +13,10 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 4000;
 
+app.use(cors());
+
 app.use("/students", router);
 app.use(GlobalError.handle);
-
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    next();
-});
 
 app.listen(PORT, () => {
     console.log("Server is running on ", PORT);
